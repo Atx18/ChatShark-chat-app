f12eed24e7cf5d1aaf82c3e4c3ce9de5732370f7
Atriavo <atriavo18@gmail.com>
2024-06-20 18:02:50 +0530

    2nd part completed-ui design

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BiLogOut } from "react-icons/bi";
+
+const LogoutButton = () => {
+  return (
+    <div className='mt-auto'>
+     <BiLogOut className='cursor-pounter w-6 h-6'/>
+</div>
+  )
+}
+
+export default LogoutButton

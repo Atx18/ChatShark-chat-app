0917d73116161778ffe402973e47a55fc3e86873
Atriavo <atriavo18@gmail.com>
2024-06-22 12:14:42 +0530

    deployment  procedure

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+import path from "path";
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
@@ -16,7 +16,7 @@ dotenv.config();
 
 const PORT= process.env.port || 5000;
 
-
+const __dirname = path.resolve();
 
 
 app.use(express.json());  //to parse the incoming requets with JSON payloads
@@ -28,6 +28,11 @@ app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
  server.listen(PORT,()=> {

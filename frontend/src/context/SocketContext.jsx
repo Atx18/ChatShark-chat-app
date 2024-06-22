d1d2843c3b6253f634e409e8e26115312cb5beae
Atriavo <atriavo18@gmail.com>
2024-06-22 12:47:13 +0530

    all done

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ( {children} ) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000/", {
+			const socket = io("https://chatshark-chat-app-production.onrender.com", {
 				query: {
 					userId: authUser.id,
 				},
